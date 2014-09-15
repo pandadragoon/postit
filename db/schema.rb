b6b2f545a3c8691d8426e5e6db17e63ea966242f
Joshua Edwards <pandadragoon@gmail.com>
2014-09-15 16:55:41 -0500

    added comment and category models and comment associations with users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915210817) do
+ActiveRecord::Schema.define(version: 20140915215308) do
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
-    t.string  "title"
-    t.string  "url"
-    t.text    "description"
-    t.integer "user_id"
+    t.string   "title"
+    t.string   "url"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
