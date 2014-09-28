1a5e0f06eac712395694f44b1c348c72de6ea031
Joshua Edwards <pandadragoon@gmail.com>
2014-09-28 15:44:01 -0500

    added polymorphic vote with validations to posts and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,4 +14,16 @@ class CommentsController < ApplicationController
     end
   end
 
+  def vote
+    comment = Comment.find(params[:id])
+    vote = Vote.create(voteable: comment, user: current_user, vote: params[:vote])
+    
+    if vote.valid?
+      flash[:notice] = 'Your vote was counted.'
+    else
+      flash[:error] = 'You can only vote on a comment once.'
+    end
+    redirect_to :back
+  end
+
 end
